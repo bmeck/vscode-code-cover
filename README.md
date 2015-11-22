4291ad0cabc04f6397bd8f46e8c5848bf6405cc3
bradleymeck <bradley.meck@gmail.com>
2015-11-21 19:07:21 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Code Cover
+
+## coverageconfig.json
+
+```
+{
+  "mappings": ""
+}
+```
+
+Due to code coverage being applied to transformed files in JS, 
---
+++ example/out/server/coverage.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _arr = [1, 2, 3];
+for (var _i = 0; _i < _arr.length; _i++) {
+  var x = _arr[_i];
+}
+//# sourceMappingURL=coverage.js.map
